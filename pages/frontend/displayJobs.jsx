2a72c4d38cde6e8fe80f5eabdd5ba9d986f53b8e
Aarushi Bhatia <aarushibhatia073@gmail.com>
2024-10-21 23:00:40 +0530

    undo

--- pages/frontend/displayJobs.jsx
+++ pages/frontend/displayJobs.jsx
@@ -26,7 +26,7 @@ export default function DisplayJobs() {
     <>
       <NavBar />
       <div className="w-full  py-20 flex items-center md:px-8 px-2  justify-center flex-col">
-        <h1 className="px-4 mx-2 mt-8 mb-4 py-2 uppercase tracking-wider border-b-2 border-b-indigo-600 text-3xl font-semibold">
+        <h1 className="px-4 mx-2 py-2 uppercase tracking-wider border-b-2 border-b-indigo-600 text-3xl font-semibold">
           Available Jobs
         </h1>
         <div className="w-full h-full py-4 flex  overflow-y-auto  items-center justify-center flex-wrap">
