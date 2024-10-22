408f6035534549e645dd4029ba76f7c861e6f90f
shashishsoni <shshshsoni2003@tutanota.com>
2024-10-22 05:31:00 -0700

    enchance the encrytion level and store the user data in database with good level of encrytion

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -1,17 +1,13 @@
 import ConnectDB from '@/DB/connectDB';
 import User from '@/models/User';
 import Joi from 'joi';
-import { compare } from 'bcryptjs';
+import argon2 from 'argon2'; // Import Argon2 for password hashing
 import jwt from 'jsonwebtoken';
-
 
 const schema = Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().required(),
 });
-
-
-
 
 export default async (req, res) => {
     await ConnectDB();
@@ -25,15 +21,16 @@ export default async (req, res) => {
         const checkUser = await User.findOne({ email });
         if (!checkUser) return res.status(401).json({ success: false, message: "Account not Found" });
 
-        const isMatch = await compare(password, checkUser.password);
+        // Use Argon2 to verify the password
+        const isMatch = await argon2.verify(checkUser.password, password);
         if (!isMatch) return res.status(401).json({ success: false, message: "Incorrect Password" });
 
-        const token = jwt.sign({ id: checkUser._id, email: checkUser.email }, process.env.JWT_SECREAT, { expiresIn: '1d' });
-        const finalData = {token , user : checkUser}
-        return res.status(200).json({ success: true, message: "Login Successfull",  finalData})
+        const token = jwt.sign({ id: checkUser._id, email: checkUser.email }, process.env.JWT_SECRET, { expiresIn: '1d' });
+        const finalData = { token, user: checkUser };
+        return res.status(200).json({ success: true, message: "Login Successful", finalData });
 
     } catch (error) {
-        console.log('Error in register (server) => ', error);
-        return res.status(500).json({ success: false, message: "Something Went Wrong Please Retry Later !" })
+        console.error('Error in login (server) => ', error); // Log the error
+        return res.status(500).json({ success: false, message: "Something Went Wrong Please Retry Later !" });
     }
 }
