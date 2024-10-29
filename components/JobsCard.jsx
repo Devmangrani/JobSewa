f18d1de7453d326d7ccd22b875c1a3e8c4a220f2
Kehinde Balogun <balogunkehinde3@gmail.com>
2024-10-29 14:06:16 -0400

    Search Results Container Overflow Bug

--- components/JobsCard.jsx
+++ components/JobsCard.jsx
@@ -9,13 +9,13 @@ export default function JobsCard({ job, posted }) {
   return (
     <div
       key={job._id}
-      className="w-full transition-all duration-1000  md:w-5/12 m-4 border hover:shadow-xl rounded p-4 md:flex md:flex-wrap"
+      className="w-full mx-auto transition-all duration-1000 shadow-2xl p-2 rounded-md"
     >
-      <div className="mb-4 flex lg:flex-col items-center justify-center py-2">
+      <div className="mx-auto justify-center p-2 flex items-center">
         <Image
           width={70}
           height={70}
-          className="flex rounded-full "
+          className="rounded-full "
           src={
             job?.user?.image ||
             "https://xsgames.co/randomusers/avatar.php?g=male"
@@ -34,25 +34,23 @@ export default function JobsCard({ job, posted }) {
       <div className="mb-4 flex min-w-fit items-start justify-center py-2 flex-col">
         <div className="flex px-2 py-2 items-center justify-center ">
           <BsDot className="text-4xl font-extrabold text-sky-700" />
-          <h1 className="text-lg text-gray-900 w-20">Salary</h1>
-          <p className="text-base  font-semibold">
-            :&nbsp;&nbsp;&nbsp;₹{job?.salary}/ month
+          <h1 className="text-lg text-gray-900 w-20">Salary :</h1>
+          <p className="text-base font-semibold">
+            &nbsp;&nbsp;&nbsp;₹{job?.salary}/ month
           </p>
         </div>
-        <div className="flex px-2 py-2 items-center  justify-center">
+        <div className="flex px-2 py-2 items-center justify-center">
           <BsDot className="text-4xl font-extrabold text-sky-700" />
-          <h1 className="text-lg text-gray-900 w-20">Deadline</h1>
+          <h1 className="text-lg text-gray-900 w-20">Deadline {":"}</h1>
           <p className="text-base  font-semibold">
-            {":"}&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;
             {new Date(`${job?.job_deadline}`).toLocaleDateString("en-GB")}
           </p>
         </div>
       </div>
-      <div className="mb-4 flex flex-col md:flex-wrap md:flex-row w-full justify-between  items-center ">
-        <div className="mb-4 flex  items-start justify-center py-2 flex-col">
-          <div className="flex px-6 rounded-2xl py-1 items-center justify-center bg-indigo-200 text-indigo-900  ">
-            <p>{job?.title} </p>
-          </div>
+      <div className="flex flex-col gap-5">
+        <div className="text-center w-[50%] mx-auto px-6 rounded-2xl py-1 bg-indigo-200 text-indigo-900 capitalize">
+          <p>{job?.title} </p>
         </div>
         {posted ? (
           <button
@@ -64,7 +62,7 @@ export default function JobsCard({ job, posted }) {
         ) : (
           <button
             onClick={() => router.push(`/frontend/jobDetails/${job?._id}`)}
-            className="my-2 py-2 px-4 cursor-pointer border border-sky-700   rounded flex items-center justify-center transition-all duration-700 hover:bg-indigo-600 hover:text-white text-sky-700 font-semibold"
+            className="my-2 py-2 px-4 cursor-pointer border border-sky-700 rounded-md flex items-center justify-center transition-all duration-700 hover:bg-indigo-600 hover:text-white text-sky-700 font-semibold"
           >
             View Detail <AiOutlineArrowRight className="mx-2 text-xl" />
           </button>
