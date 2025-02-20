d7ef2da1c3b920d30cb4819b2cb6da4836e9c452
devmangrani <devmangrani5@gmail.com>
2025-02-20 11:08:29 +0530

    Made contact us page responsive in small screens

--- rodella-website/src/components/Navbar.jsx
+++ rodella-website/src/components/Navbar.jsx
@@ -19,6 +19,10 @@ const Navbar = () => {
     setIsAircraftOpen(!isAircraftOpen);
   };
 
+  const closeMobileMenu = () => {
+    setMobileDrawerOpen(false);
+  };
+
   return (
     <nav className="sticky top-0 py-3 bg-gray-950/75">
       <div className="container px-3 mx-auto relative text-sm">
@@ -29,8 +33,10 @@ const Navbar = () => {
                 className="h-10 w-10 mr-2 bg-gray-200 rounded-full"
                 src={logo}
                 alt="logo"
-              ></img>
-              <span className="text-2xl tracking right text-white">Rodella</span>
+              />
+              <span className="text-2xl tracking right text-white">
+                Rodella
+              </span>
             </Link>
           </div>
           <ul className="hidden lg:flex ml-14 space-x-8">
@@ -45,42 +51,32 @@ const Navbar = () => {
                 }}
                 onMouseLeave={() => {
                   if (item.submenu) {
-                    //bug at the submenu
-                    // Check if mouse is still within the submenu before closing
-                    // const submenu = document.querySelector(`#${item.label}-submenu`); // Use ID
-                    // if (submenu && !submenu.matches(':hover')) { // Check if submenu is NOT hovered
-                    //     setIsAircraftOpen(false);
-                    // }
+                    setIsAircraftOpen(false);
                   }
-                  setIsAircraftOpen(false);
                 }}
               >
-                <a
-                  href={item.href}
+                <Link
+                  to={item.href}
                   className="text-white hover:text-gray-300 px-3 py-2 rounded flex items-center"
-                  onClick={(e) => {
-                    if (item.submenu) {
-                      e.preventDefault();
-                    }
-                  }}
+                  onClick={closeMobileMenu} // Close mobile menu on click
                 >
                   {item.label}
-                </a>
-                {item.submenu &&
-                  isAircraftOpen && ( // Check if submenu exists
-                    <ul className="absolute z-10 bg-gray-800 rounded-md shadow-lg mt-2 w-25 left-0">
-                      {item.submenu.map((subItem) => (
-                        <li key={subItem.label}>
-                          <a
-                            href={subItem.href}
-                            className="block px-4 py-2 text-orange-400 hover:bg-gray-600 whitespace-nowrap hover:rounded-md"
-                          >
-                            {subItem.label}
-                          </a>
-                        </li>
-                      ))}
-                    </ul>
-                  )}
+                </Link>
+                {item.submenu && isAircraftOpen && (
+                  <ul className="absolute z-10 bg-gray-800 rounded-md shadow-lg mt-2 w-25 left-0">
+                    {item.submenu.map((subItem) => (
+                      <li key={subItem.label}>
+                        <Link
+                          to={subItem.href}
+                          className="block px-4 py-2 text-orange-400 hover:bg-gray-600 whitespace-nowrap hover:rounded-md"
+                          onClick={closeMobileMenu} // Close mobile menu on click
+                        >
+                          {subItem.label}
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                )}
               </li>
             ))}
           </ul>
@@ -89,6 +85,7 @@ const Navbar = () => {
             <Link
               to="/contact"
               className="bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md"
+              onClick={closeMobileMenu} // Close mobile menu on click
             >
               Contact Us
             </Link>
@@ -96,44 +93,43 @@ const Navbar = () => {
           <div className="lg:hidden md:flex flex-col justify-end">
             <button onClick={toggleNavbar}>
               {mobileDrawerOpen ? (
-                <img src={closeIcon} alt="close-menu" className="h-6 w-6"></img>
+                <img src={closeIcon} alt="close-menu" className="h-6 w-6" />
               ) : (
-                <img src={menuIcon} alt="open-menu" className="h-6 w-6"></img>
+                <img src={menuIcon} alt="open-menu" className="h-6 w-6" />
               )}
             </button>
           </div>
         </div>
         {mobileDrawerOpen && (
-          <div className="fixed right-0 z-20 bg-neutral-900 w-full p-12 flex flex-col justify-center items-center lg:hidden">
+          <div
+            className="fixed right-0 z-20 bg-neutral-900 w-full p-12 flex flex-col justify-center items-center lg:hidden"
+            onClick={closeMobileMenu} // Close mobile menu on click outside
+          >
             <ul className="flex flex-col items-center">
-              {" "}
-              {/* Vertical alignment */}
               {navItems.map((item) => (
                 <li key={item.label} className="py-4 w-full text-center">
-                  {" "}
-                  {/* Full width, centered text */}
-                  <a
-                    href={item.href}
-                    onClick={item.submenu ? handleAircraftsClickMobile : null} // Handle click for Aircrafts
-                    className="text-white hover:text-gray-300 block" // Make links block elements
+                  <Link
+                    to={item.href}
+                    onClick={closeMobileMenu} // Close mobile menu on click
+                    className="text-white hover:text-gray-300 block"
                   >
                     {item.label}
-                  </a>
-                  {item.submenu &&
-                    isAircraftOpen && ( // Mobile submenu
-                      <ul className="mt-2 space-y-2">
-                        {item.submenu.map((subItem) => (
-                          <li key={subItem.label}>
-                            <a
-                              href={subItem.href}
-                              className="text-white hover:text-gray-300 block"
-                            >
-                              {subItem.label}
-                            </a>
-                          </li>
-                        ))}
-                      </ul>
-                    )}
+                  </Link>
+                  {item.submenu && isAircraftOpen && (
+                    <ul className="mt-2 space-y-2">
+                      {item.submenu.map((subItem) => (
+                        <li key={subItem.label}>
+                          <Link
+                            to={subItem.href}
+                            className="text-white hover:text-gray-300 block"
+                            onClick={closeMobileMenu} // Close mobile menu on click
+                          >
+                            {subItem.label}
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  )}
                 </li>
               ))}
             </ul>
@@ -141,6 +137,7 @@ const Navbar = () => {
               <Link
                 to="/contact"
                 className="bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md"
+                onClick={closeMobileMenu} // Close mobile menu on click
               >
                 Contact Us
               </Link>
