14344febc3ae40cfe6de49553833b3cd5fa968c6
aadityaraj1611 <aadityaraj1611@gmail.com>
2025-02-24 11:37:33 +0530

    added navbar inside the main section

--- rodella-website/src/App.jsx
+++ rodella-website/src/App.jsx
@@ -1,19 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
-<<<<<<< HEAD
 import Homepage from "./pages/Homepage";
 import ContactUs from "./pages/ContactUs";
 import AboutUs from "./pages/AboutUs";
 import Aircrafts from "./pages/Aircrafts";
 import Services from "./pages/Services";
-=======
-import Homepage from "./components/Homepage";
-import ContactUs from "./components/ContactUs";
-import AboutUs from "./components/AboutUs";
-import Aircrafts from "./components/Aircrafts";
-import Services from "./components/Services";
->>>>>>> 9e8f82c (add footer to the main app)
 import Footer from "./components/footer";
 
 
