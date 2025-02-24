87d68d91c25df8efe8ee55639c3f10d75c0fbfb9
aadityaraj1611 <aadityaraj1611@gmail.com>
2025-02-24 11:39:42 +0530

    commented out the composite mart navitem

--- rodella-website/src/constants/index.jsx
+++ rodella-website/src/constants/index.jsx
@@ -1,13 +1,13 @@
 export const navItems = [
   { label: "Aircrafts", href: "/aircrafts",
-    // submenu: [
+    // submenu: [ // will be added later on
     //   {label: "Aircraft 1", href: "#"},
     //   {label: "Aircraft 2", href: "#"},
     //   {label: "Aircraft 3", href: "#"},
     //   {label: "Aircraft 4", href: "#"}
     // ]
    },
-  // { label: "Composite Mart", href: "#" },
+  // { label: "Composite Mart", href: "#" }, //will be added later on
   { label: "Services", href: "/services" },
   { label: "About", href: "/about" }
 ];
