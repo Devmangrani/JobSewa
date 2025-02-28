9a14d688e00b5c0405065b2d66b51f07dc943db7
aadityaraj1611 <aadityaraj1611@gmail.com>
2025-02-28 11:54:30 +0530

    added a new Engineering page

---
+++ rodella-website/src/pages/Engineering.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/Navbar";
+
+const Engineering = () => {
+
+    return(
+        <div className="bg-black pt-2">
+             <Navbar />
+            <section className="p-50">
+                <h1 className="text-xl text-white">This is the design section</h1>
+            </section>
+        </div>
+    )
+}
+
+export default Engineering;
