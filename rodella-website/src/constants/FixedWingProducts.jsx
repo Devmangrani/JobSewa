84d7fc554270c94428c36a25415743b06ceb6229
aadityaraj1611 <aadityaraj1611@gmail.com>
2025-03-03 20:50:30 +0530

    new images and video

---
+++ rodella-website/src/constants/FixedWingProducts.jsx
@@ -0,0 +1,27 @@
+import Talon from "../assets/images/fixed-wing1.png";
+import Nova from  "../assets/images/fixed-wing2.jpg";
+import Cipher from  "../assets/images/4-1.png";
+
+const FixedWingProducts = [
+    {
+        id: 1,
+        name: "Project Talon-Q9872MV",
+        price: "rs398743",
+        img: Talon
+    },
+    {
+        id: 2,
+        name: "Nova Wing-Q3484MXV",
+        price: "rs398743",
+        img: Nova
+    },
+    {
+        id: 3,
+        name: "Cipher Flight-QW3289LV",
+        price: "rs398743",
+        img: Cipher
+    },
+
+]
+
+export default FixedWingProducts;
---
+++ rodella-website/src/constants/MultirotorProducts.jsx
@@ -0,0 +1,34 @@
+import MQ650LV from "./../assets/images/MQ650LV.jpg";
+import MQ750LV from "./../assets/images/services.jpg";
+import MQ900 from "./../assets/images/achievment.jpg";
+
+const MultirotorProducts = [
+
+    {
+        id: 1,
+        name: "Skybolt X-MQ650LV",
+        price: "rs93434",
+        img: MQ650LV
+    },
+    {
+        id: 2,
+        name: "Zephyr Dart-MQ750LV",
+        price: "rs93434",
+        img: MQ750LV
+    },
+    {
+        id: 3,
+        name: "Chimera Glide-MQ900",
+        price: "rs93434",
+        img: MQ900
+    },
+    // {
+    //     id: 4,
+    //     name: "MQ650LV",
+    //     price: "rs93434",
+    //     img: MQ650LV
+    // }
+
+]
+
+export default MultirotorProducts;
