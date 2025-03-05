0dc16c3b8db4d4b5da5c4584c6ddd8ce190f4b15
aadityaraj1611 <aadityaraj1611@gmail.com>
2025-03-05 13:38:03 +0530

    gave unique id to each menu item

--- rodella-website/src/constants/NavItems.jsx
+++ rodella-website/src/constants/NavItems.jsx
@@ -1,61 +1,44 @@
 import { href } from "react-router-dom";
 
-// export const navItems = [
-//   { label: "Aircrafts", href: "/aircrafts",
-//     submenu: [ // will be added later on
-//       {label: "Multirotors",
-//         Innersubmenu1: [
-//           {
-//             label: "asdsaikhfas", href: "/multirotor/1"
-//           }
-//         ]
-//       },
-//       {label: "FixedWings",
-//         Innersubmenu2: [
-//           {
-//             label: "Nova flight-RX232QW", href: "/fixedwing/1"
-//           }
-//         ]
-//       },
-//     ]
-//    },
-//   { label: "Engineering", href: "/engineering" }, //will be added later on
-//   // { label: "Services", href: "/services" },
-//   { label: "About", href: "/about" }
-// ];
-
 export const Menus = [
   {
+    id: 1,
     name: "Aircrafts",
     subMenuHeading: ["Multirotor", "FixedWings"],
     href: "/aircrafts",
     subMenu: [
       {
+        id: 1,
         name: "X-MQ650LV",
         desc: "Responsive design",
         href: "/multirotor/1"
       },
       {
+        id: 2,
         name: "Project Talon-Q9872MV",
         desc: "Management content",
         href: "/fixedwing/1"
       },
       {
+        id: 3,
         name: "MQ750LV",
         desc: "Site control",
         href: "/multirotor/2"
       },
       {
+        id: 4,
         name: "Nova Wing-Q3484MXV",
         desc: "Management content",
         href: "/fixedwing/2"
       },
       {
+        id: 5,
         name: "X-MQ900",
         desc: "Link pages",
         href: "/multirotor/3"
       },
       {
+        id: 6,
         name: "Cipher Flight-QW3289LV",
         desc: "Management content",
         href: "/fixedwing/3"
@@ -65,16 +48,19 @@ export const Menus = [
   },
 
   {
+    id: 2,
     name: "Engineering",
     href: "/engineering",
   },
 
   {
+    id: 3,
     name: "Blog",
     href: "/blog",
   },
 
   {
+    id: 4,
     name: "About",
     href: "/about",
   },
