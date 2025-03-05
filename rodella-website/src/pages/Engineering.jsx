204d6c7510245be39c45178752607f88b9a33d1e
aadityaraj1611 <aadityaraj1611@gmail.com>
2025-03-05 13:43:47 +0530

    desing of the engineering page is complete

--- rodella-website/src/pages/Engineering.jsx
+++ rodella-website/src/pages/Engineering.jsx
@@ -3,45 +3,53 @@ import ManufacturingVideo from "../assets/videos/demo1.mp4";
 import DesignImg1 from "./../assets/images/services.jpg";
 import DesignImg2 from "./../assets/images/fixed-wing1.png";
 import PrototypeImg1 from "./../assets/images/design.jpg";
-import PrototypeImg2 from "./../assets/images/MQ650LV.jpg"
+import PrototypeImg2 from "./../assets/images/MQ650LV.jpg";
 import PrototypeImg3 from "./../assets/images/fixed-wing2.jpg";
 import React, { useRef } from "react";
+import HorizontalScrollCarousel from "../components/HorizontalScrollCarousel";
+import { ArrowUp } from "lucide-react";
 
 
 const Engineering = () => {
 
+    const section0Ref = useRef(null);
     const section1Ref = useRef(null);
     const section2Ref = useRef(null);
     const section3Ref = useRef(null);
     const section4Ref = useRef(null);
 
     const scrollToSection = (elementRef) => {
-        if(elementRef.current) {
-            elementRef.current.scrollIntoView({behavior: 'smooth'});
+        if (elementRef.current) {
+            elementRef.current.scrollIntoView({ behavior: 'smooth' });
         }
     }
 
     return (
         <div className="bg-black pt-2">
             <Navbar />
-            <section className="px-50 py-30">
+            <section ref={section0Ref} id="section0" className="px-50 py-30">
                 <h1 className="text-white text-5xl text-center">Want Some Engineering Wizardry For Yourself?</h1>
                 <p className="text-neutral-400 text-center mt-5 text-xl">All our Drones are designed and developed in house.</p>
+
                 <div className="text-white mt-10 flex flex-row justify-center items-center gap-10">
                     <div className="text-xl border-2 border-neutral-500 bg-black mb-5 rounded-full px-5 py-2 text-white cursor-pointer"
                         onClick={() => scrollToSection(section1Ref)}
-                        >
+                    >
                         Design</div>
                     <div className="text-xl border-2 border-neutral-500 bg-black mb-5 rounded-full px-5 py-2 text-white cursor-pointer"
                         onClick={() => scrollToSection(section2Ref)}
-                        >
+                    >
                         Prototyping</div>
                     <div className="text-xl border-2 border-neutral-500 bg-black mb-5 rounded-full px-5 py-2 text-white cursor-pointer"
                         onClick={() => scrollToSection(section3Ref)}
-                        >
+                    >
                         Simulation</div>
-                    <div className="text-xl border-2 border-neutral-500 bg-black mb-5 rounded-full px-5 py-2 text-white">Airframe Integration</div>
+                    <div className="text-xl border-2 border-neutral-500 bg-black mb-5 rounded-full px-5 py-2 text-white cursor-pointer"
+                        onClick={() => scrollToSection(section4Ref)}
+                    >
+                        Airframe Integration</div>
                 </div>
+
                 <div className="flex justify-center items-center">
                     <video
                         src={ManufacturingVideo}
@@ -137,7 +145,62 @@ const Engineering = () => {
 
             <section ref={section3Ref} id="section3" className="px-50 py-30">
                 <h1 className="text-white text-5xl text-center underline">Simulation</h1>
-                <p className="text-neutral-400 text-xl text-center mt-3">From static testing to CFD, to model analysis, we do it for ourselves, We will do it for you</p>
+                <p className="text-neutral-400 text-xl text-center mt-3 mb-5">From static testing to CFD, to model analysis, we do it for ourselves, We will do it for you</p>
+                <div>
+                    <HorizontalScrollCarousel />
+                </div>
+            </section>
+
+            <section ref={section4Ref} id="section4" className="px-50 py-30">
+                <h1 className="text-5xl text-white text-center underline">Airframe Integration</h1>
+                <p className="text-neutral-400 text-xl text-center mt-3 mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo dolor dignissimos, eius quasi iste modi.</p>
+                <div className="px-20">
+                    <img src={DesignImg2} className="h-150 w-full object-cover"></img>
+                </div>
+                <div className="px-20 my-10">
+                    <div className="flex flex-row justify-between">
+                        <div>
+                            <h2 className="text-3xl text-white">Component Placement & Mounting</h2>
+                            <ul className="list-disc ml-5 text-2xl text-neutral-400 mt-5">
+                                <li>Strategic Placement:</li>
+                                <li>Secure Mounting:</li>
+                                <li>Vibration Isolation:</li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h2 className="text-3xl text-white">Wiring and Connections</h2>
+                            <ul className="list-disc ml-5 text-2xl text-neutral-400 mt-5">
+                                <li>Clean Wiring</li>
+                                <li>Secure Connections</li>
+                                <li>Cable Management</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="flex flex-row justify-between mt-10">
+                        <div>
+                            <h2 className="text-3xl text-white">Payload Integration</h2>
+                            <ul className="list-disc ml-5 text-2xl text-neutral-400 mt-5">
+                                <li>Payload Mounting</li>
+                                <li>Payload Power and Data Connections</li>
+                                <li>Payload Integration with Flight Controller</li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h2 className="text-3xl text-white">Testing and Validation</h2>
+                            <ul className="list-disc ml-5 text-2xl text-neutral-400 mt-5">
+                                <li>Ground Testing</li>
+                                <li>Vibration Testing</li>
+                                <li>Flight Testing</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className="w-full flex items-center justify-center cursor-pointer" onClick={() => scrollToSection(section0Ref)}>
+                <h2 className="text-white text-3xl text-center mt-30 border-[2px] inline-flex px-6 py-3 rounded-full items-center justify-center">
+                    Go Back
+                    <ArrowUp size={30} color="white"/>
+                    </h2>
+                </div>
             </section>
         </div>
     )
