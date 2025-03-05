f5c97a638d762f91800811b52749e0ae2be7cb67
aadityaraj1611 <aadityaraj1611@gmail.com>
2025-03-05 13:40:26 +0530

    created a new data array for the horizontalscrollcarousel in the engineering page simulation section

---
+++ rodella-website/src/constants/EngineeringSimulationData.jsx
@@ -0,0 +1,44 @@
+import CFDimg from "./../assets/images/homepage-bg2.jpeg";
+ 
+// this array contains the data for the simulation section in the Enginering page
+
+const cards = [
+
+    {
+        url: CFDimg,
+        title: "Title 1",
+        id: 1,
+    },
+    {
+        url: CFDimg,
+        title: "Title 2",
+        id: 2,
+    },
+    {
+        url: CFDimg,
+        title: "Title 3",
+        id: 3,
+    },
+    {
+        url: CFDimg,
+        title: "Title 4",
+        id: 4,
+    },
+    {
+        url: CFDimg,
+        title: "Title 5",
+        id: 5,
+    },
+    {
+        url: CFDimg,
+        title: "Title 6",
+        id: 6,
+    },
+    {
+        url: CFDimg,
+        title: "Title 7",
+        id: 7,
+    },
+];
+
+export default cards;
